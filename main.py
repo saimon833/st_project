ffc493866f8981e2393ce9fe86ce793085eff9c9
saimon833 <74502684+saimon833@users.noreply.github.com>
2022-12-30 17:59:03 +0100

    dupa

--- main.py
+++ main.py
@@ -2,16 +2,9 @@ import archinstall, getpass
 
 # Unmount and close previous runs
 archinstall.sys_command(f'umount -R /mnt', surpress_errors=True)
-archinstall.sys_command(f'cryptsetup close /dev/mapper/luksloop', surpress_errors=True)
 
 # Select a harddrive and a disk password
 harddrive = archinstall.select_disk(archinstall.all_disks())
-while (disk_password := getpass.getpass(prompt='Enter disk encryption password (leave blank for no encryption): ')):
-	disk_password_verification = getpass.getpass(prompt='And one more time for verification: ')
-	if disk_password != disk_password_verification:
-		archinstall.log(' * Passwords did not match * ', bg='black', fg='red')
-		continue
-	break
 
 def perform_installation(device, boot_partition):
 	hostname = input('Desired hostname for the installation: ')
@@ -46,28 +39,13 @@ def perform_installation(device, boot_partition):
 				installation.user_set_pw('root', root_pw)
 				break
 
-			aur = input('Would you like AUR support? (leave blank for no): ')
-			if len(aur.strip()):
-				archinstall.log(' - AUR support provided by yay (https://aur.archlinux.org/packages/yay/)', bg='black', fg='white')
-				installation.add_AUR_support()
 
 with archinstall.Filesystem(harddrive, archinstall.GPT) as fs:
-	# Use partitioning helper to set up the disk partitions.
-	if disk_password:
-		fs.use_entire_disk('luks2')
-	else:
-		fs.use_entire_disk('ext4')
+	fs.use_entire_disk('ext4')
 
 	if harddrive.partition[1].size == '512M':
 		raise OSError('Trying to encrypt the boot partition for petes sake..')
 	harddrive.partition[0].format('fat32')
 
-	if disk_password:
-		# First encrypt and unlock, then format the desired partition inside the encrypted part.
-		with archinstall.luks2(harddrive.partition[1], 'luksloop', disk_password) as unlocked_device:
-			unlocked_device.format('btrfs')
-			
-			perform_installation(unlocked_device, harddrive.partition[0])
-	else:
-		harddrive.partition[1].format('ext4')
-		perform_installation(harddrive.partition[1], harddrive.partition[0])
+	harddrive.partition[1].format('ext4')
+	perform_installation(harddrive.partition[1], harddrive.partition[0])
