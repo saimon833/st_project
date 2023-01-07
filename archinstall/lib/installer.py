c4d4b6b2a9dc063bdce792bbf08fb9899f336e26
saimon833 <74502684+saimon833@users.noreply.github.com>
2023-01-07 16:58:10 +0100

    Newer version

--- archinstall/lib/installer.py
+++ archinstall/lib/installer.py
@@ -1,128 +1,161 @@
 import os, stat
-import re
+
 from .exceptions import *
 from .disk import *
 from .general import *
 from .user_interaction import *
 from .profiles import Profile
 
+class Installer():
+	def __init__(self, partition, boot_partition, *, profile=None, mountpoint='/mnt', hostname='ArchInstalled'):
+		self.profile = profile
+		self.hostname = hostname
+		self.mountpoint = mountpoint
 
-class Installer:
-    def __init__(self, partition, *, profile=None, mountpoint='/mnt', hostname='ArchInstalled'):
-        self.profile = profile
-        self.hostname = hostname
-        self.mountpoint = mountpoint
+		self.partition = partition
+		self.boot_partition = boot_partition
 
-        self.partition = partition
+	def __enter__(self, *args, **kwargs):
+		self.partition.mount(self.mountpoint)
+		os.makedirs(f'{self.mountpoint}/boot', exist_ok=True)
+		self.boot_partition.mount(f'{self.mountpoint}/boot')
+		return self
 
-    def __enter__(self, *args, **kwargs):
-        self.partition.mount(self.mountpoint)
-        return self
+	def __exit__(self, *args, **kwargs):
+		# b''.join(sys_command(f'sync')) # No need to, since the underlaying fs() object will call sync.
+		# TODO: https://stackoverflow.com/questions/28157929/how-to-safely-handle-an-exception-inside-a-context-manager
+		if len(args) >= 2 and args[1]:
+			raise args[1]
+		log('Installation completed without any errors.', bg='black', fg='green')
+		return True
 
-    def __exit__(self, *args, **kwargs):
-        # b''.join(sys_command(f'sync')) # No need to, since the underlaying fs() object will call sync.
-        # TODO: https://stackoverflow.com/questions/28157929/how-to-safely-handle-an-exception-inside-a-context-manager
-        if len(args) >= 2 and args[1]:
-            raise args[1]
-        log('Installation completed without any errors.', bg='black', fg='green')
-        return True
+	def pacstrap(self, *packages, **kwargs):
+		if type(packages[0]) in (list, tuple): packages = packages[0]
+		log(f'Installing packages: {packages}')
 
-    def pacstrap(self, *packages):
-        if type(packages[0]) in (list, tuple): packages = packages[0]
-        log(f'Installing packages: {packages}')
+		if (sync_mirrors := sys_command('/usr/bin/pacman -Syy')).exit_code == 0:
+			if (pacstrap := sys_command(f'/usr/bin/pacstrap {self.mountpoint} {" ".join(packages)}', **kwargs)).exit_code == 0:
+				return True
+			else:
+				log(f'Could not strap in packages: {pacstrap.exit_code}')
+		else:
+			log(f'Could not sync mirrors: {sync_mirrors.exit_code}')
 
-        if (sync_mirrors := sys_command('/usr/bin/pacman -Syy')).exit_code == 0:
-            if (pacstrap := sys_command(f'/usr/bin/pacstrap {self.mountpoint} {" ".join(packages)}')).exit_code == 0:
-                return True
-            else:
-                log(f'Could not strap in packages: {pacstrap.exit_code}')
-        else:
-            log(f'Could not sync mirrors: {sync_mirrors.exit_code}')
+	def genfstab(self, flags='-Pu'):
+		o = b''.join(sys_command(f'/usr/bin/genfstab -pU {self.mountpoint} >> {self.mountpoint}/etc/fstab'))
+		if not os.path.isfile(f'{self.mountpoint}/etc/fstab'):
+			raise RequirementError(f'Could not generate fstab, strapping in packages most likely failed (disk out of space?)\n{o}')
+		return True
 
-    def minimal_installation(self):
-        return self.pacstrap(
-            'base base-devel linux linux-firmware btrfs-progs efibootmgr nano wpa_supplicant dialog grub'.split(' '))
+	def set_hostname(self, hostname=None):
+		if not hostname: hostname = self.hostname
+		with open(f'{self.mountpoint}/etc/hostname', 'w') as fh:
+			fh.write(self.hostname + '\n')
 
-    def generate_fstab(self,boot,root):
-        with open(f'{self.mountpoint}/etc/fstab', 'w') as fstab:
-            fstab.write(f'{root}#\n')
-            uuid = sys_command(f'blkid -s UUID -o value {root}').decode()
-            fstab.write(f'UUID="{uuid}\t /\t ext4\t rw,relatime \t0 1\n')
-            fstab.write(f'{boot}#\n')
-            uuid = sys_command(f'blkid -s UUID -o value {boot}').decode()
-            fstab.write(f'UUID="{uuid}\t /boot\t ext4\t rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,utf8,errors=remount-ro\t 0 2\n')
+	def set_locale(self, locale, encoding='UTF-8'):
+		with open(f'{self.mountpoint}/etc/locale.gen', 'a') as fh:
+			fh.write(f'{locale} {encoding}\n')
+		with open(f'{self.mountpoint}/etc/locale.conf', 'w') as fh:
+			fh.write(f'LANG={locale}\n')
+		sys_command(f'/usr/bin/arch-chroot {self.mountpoint} locale-gen')
 
-    def add_bootloader(self, boot_partition, boot, root):
-        log(f'Adding bootloader to {boot_partition}')
-        os.makedirs(f'{self.mountpoint}/boot', exist_ok=True)
-        boot_partition.mount(f'{self.mountpoint}/boot')
-        self.generate_fstab(boot, root)
-        o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} grub-install --target=x86_64-efi '
-                                 f'--efi-directory=/boot --bootloader-id=GRUB'))
-        o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} grub-mkconfig -o /boot/grub/grub.cfg'))
+	def minimal_installation(self):
+		self.pacstrap('base base-devel linux linux-firmware btrfs-progs efibootmgr nano'.split(' '))
+		self.genfstab()
 
-    #		with open(f'{self.mountpoint}/boot/loader/loader.conf', 'w') as loader:
-    #			loader.write('default arch\n')
-    #			loader.write('timeout 5\n')
-    #
-    #		## For some reason, blkid and /dev/disk/by-uuid are not getting along well.
-    #		## And blkid is wrong in terms of LUKS.
-    #		#UUID = sys_command('blkid -s PARTUUID -o value {drive}{partition_2}'.format(**args)).decode('UTF-8').strip()
-    #		with open(f'{self.mountpoint}/boot/loader/entries/arch.conf', 'w') as entry:
-    #			entry.write('title Arch Linux\n')
-    #			entry.write('linux /vmlinuz-linux\n')
-    #			entry.write('initrd /initramfs-linux.img\n')
-    #			## blkid doesn't trigger on loopback devices really well,
-    #			## so we'll use the old manual method until we get that sorted out.
-    #			# UUID = simple_command(f"blkid -s PARTUUID -o value /dev/{os.path.basename(args['drive'])}{args['partitions']['2']}").decode('UTF-8').strip()
-    #			# entry.write('options root=PARTUUID={UUID} rw intel_pstate=no_hwp\n'.format(UUID=UUID))
-    #			for root, folders, uids in os.walk('/dev/disk/by-uuid'):
-    #				for uid in uids:
-    #					real_path = os.path.realpath(os.path.join(root, uid))
-    #					if not os.path.basename(real_path) == os.path.basename(partition.path): continue
-    #
-    #					entry.write(f'options cryptdevice=UUID={uid}:luksdev root=/dev/mapper/luksdev rw intel_pstate=no_hwp\n')
-    #					return True
-    #				break
-    #		raise RequirementError(f'Could not identify the UUID of {partition}, there for {self.mountpoint}/boot/loader/entries/arch.conf will be broken until fixed.')
+		with open(f'{self.mountpoint}/etc/fstab', 'a') as fstab:
+			fstab.write('\ntmpfs /tmp tmpfs defaults,noatime,mode=1777 0 0\n') # Redundant \n at the start? who knoes?
 
-    def add_additional_packages(self, *packages):
-        self.pacstrap(*packages)
+		## TODO: Support locale and timezone
+		#os.remove(f'{self.mountpoint}/etc/localtime')
+		#sys_command(f'/usr/bin/arch-chroot {self.mountpoint} ln -s /usr/share/zoneinfo/{localtime} /etc/localtime')
+		#sys_command('/usr/bin/arch-chroot /mnt hwclock --hctosys --localtime')
+		self.set_hostname()
+		self.set_locale('en_US.UTF-8')
 
-    def install_profile(self, profile):
-        profile = Profile(self, profile)
+		# TODO: Use python functions for this
+		sys_command(f'/usr/bin/arch-chroot {self.mountpoint} chmod 700 /root')
 
-        log(f'Installing network profile {profile}')
-        profile.install()
+		if self.partition.filesystem == 'btrfs':
+			with open(f'{self.mountpoint}/etc/mkinitcpio.conf', 'w') as mkinit:
+				## TODO: Don't replace it, in case some update in the future actually adds something.
+				mkinit.write('MODULES=(btrfs)\n')
+				mkinit.write('BINARIES=(/usr/bin/btrfs)\n')
+				mkinit.write('FILES=()\n')
+				mkinit.write('HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)\n')
+			sys_command(f'/usr/bin/arch-chroot {self.mountpoint} mkinitcpio -p linux')
 
-    def user_create(self, user: str, password=None, groups=[]):
-        log(f'Creating user {user}')
-        o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} useradd -m -G wheel {user}'))
-        if password:
-            self.user_set_pw(user, password)
-        if groups:
-            for group in groups:
-                o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} gpasswd -a {user} {group}'))
+		return True
 
-    def user_set_pw(self, user, password):
-        log(f'Setting password for {user}')
-        o = b''.join(
-            sys_command(f"/usr/bin/arch-chroot {self.mountpoint} sh -c \"echo '{user}:{password}' | chpasswd\""))
-        pass
+	def add_bootloader(self):
+		log(f'Adding bootloader to {self.boot_partition}')
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} bootctl --no-variables --path=/boot install'))
+		with open(f'{self.mountpoint}/boot/loader/loader.conf', 'w') as loader:
+			loader.write('default arch\n')
+			loader.write('timeout 5\n')
 
-    def add_AUR_support(self):
-        log(f'Building and installing yay support into {self.mountpoint}')
-        self.add_additional_packages(['git', 'base-devel'])  # TODO: Remove if not explicitly added at one point
-        o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "useradd -m -G wheel aibuilder"'))
-        o = b''.join(sys_command(
-            f"/usr/bin/sed -i 's/# %wheel ALL=(ALL) NO/%wheel ALL=(ALL) NO/' {self.mountpoint}/etc/sudoers"))
+		## For some reason, blkid and /dev/disk/by-uuid are not getting along well.
+		## And blkid is wrong in terms of LUKS.
+		#UUID = sys_command('blkid -s PARTUUID -o value {drive}{partition_2}'.format(**args)).decode('UTF-8').strip()
+		with open(f'{self.mountpoint}/boot/loader/entries/arch.conf', 'w') as entry:
+			entry.write('title Arch Linux\n')
+			entry.write('linux /vmlinuz-linux\n')
+			entry.write('initrd /initramfs-linux.img\n')
+			## blkid doesn't trigger on loopback devices really well,
+			## so we'll use the old manual method until we get that sorted out.
+			
 
-        o = b''.join(sys_command(
-            f'/usr/bin/arch-chroot {self.mountpoint} sh -c "su - aibuilder -c \\"(cd /home/aibuilder; git clone https://aur.archlinux.org/yay.git)\\""'))
-        o = b''.join(sys_command(
-            f'/usr/bin/arch-chroot {self.mountpoint} sh -c "chown -R aibuilder.aibuilder /home/aibuilder/yay"'))
-        o = b''.join(sys_command(
-            f'/usr/bin/arch-chroot {self.mountpoint} sh -c "su - aibuilder -c \\"(cd /home/aibuilder/yay; makepkg -si --noconfirm)\\" >/dev/null"'))
+			if self.partition.encrypted:
+				for root, folders, uids in os.walk('/dev/disk/by-uuid'):
+					for uid in uids:
+						real_path = os.path.realpath(os.path.join(root, uid))
+						if not os.path.basename(real_path) == os.path.basename(self.partition.real_device): continue
 
-        o = b''.join(
-            sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "userdel aibuilder; rm -rf /hoem/aibuilder"'))
+						entry.write(f'options cryptdevice=UUID={uid}:luksdev root=/dev/mapper/luksdev rw intel_pstate=no_hwp\n')
+						return True
+					break
+			else:
+				for root, folders, uids in os.walk('/dev/disk/by-partuuid'):
+					for uid in uids:
+						real_path = os.path.realpath(os.path.join(root, uid))
+						if not os.path.basename(real_path) == os.path.basename(self.partition.path): continue
+
+						entry.write(f'options root=PARTUUID={uid} rw intel_pstate=no_hwp\n')
+						return True
+					break
+		raise RequirementError(f'Could not identify the UUID of {self.partition}, there for {self.mountpoint}/boot/loader/entries/arch.conf will be broken until fixed.')
+
+	def add_additional_packages(self, *packages):
+		self.pacstrap(*packages)
+
+	def install_profile(self, profile):
+		profile = Profile(self, profile)
+
+		log(f'Installing network profile {profile}')
+		profile.install()
+
+	def user_create(self, user :str, password=None, groups=[]):
+		log(f'Creating user {user}')
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} useradd -m -G wheel {user}'))
+		if password:
+			self.user_set_pw(user, password)
+		if groups:
+			for group in groups:
+				o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} gpasswd -a {user} {group}'))
+
+	def user_set_pw(self, user, password):
+		log(f'Setting password for {user}')
+		o = b''.join(sys_command(f"/usr/bin/arch-chroot {self.mountpoint} sh -c \"echo '{user}:{password}' | chpasswd\""))
+		pass
+
+	def add_AUR_support(self):
+		log(f'Building and installing yay support into {self.mountpoint}')
+		self.add_additional_packages(['git', 'base-devel']) # TODO: Remove if not explicitly added at one point
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "useradd -m -G wheel aibuilder"'))
+		o = b''.join(sys_command(f"/usr/bin/sed -i 's/# %wheel ALL=(ALL) NO/%wheel ALL=(ALL) NO/' {self.mountpoint}/etc/sudoers"))
+
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "su - aibuilder -c \\"(cd /home/aibuilder; git clone https://aur.archlinux.org/yay.git)\\""'))
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "chown -R aibuilder.aibuilder /home/aibuilder/yay"'))
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "su - aibuilder -c \\"(cd /home/aibuilder/yay; makepkg -si --noconfirm)\\" >/dev/null"'))
+
+		o = b''.join(sys_command(f'/usr/bin/arch-chroot {self.mountpoint} sh -c "userdel aibuilder; rm -rf /hoem/aibuilder"'))
